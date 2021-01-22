ff8329759bf3357882fa86b6149fe0252c387978
leiyuwork <g51583255>
2021-01-22 20:17:06 +0900

    reorder columns order by year (2020-2019-2018...)

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ for file in files:
                    date_format='%Y%m%d')
 
     Fiscal_year = IS_data.iloc[13].values.tolist()
-    Fiscal_year_final = [Fiscal_year]
+    Fiscal_year_final = [Fiscal_year[:0:-1]]
     Fiscal = pd.DataFrame(Fiscal_year_final)
     Fiscal.iloc[0] = row_string + '_' + Fiscal.iloc[0].astype(str)
     print(Fiscal)
     Fiscal.to_csv(path + '\\' + "Fiscal_" + row_string + ".csv", mode='a', index=False, header=None, date_format='%Y%m%d')
 
-    Item_value = IS_data[IS_data.iloc[:, 0].str.contains(row_string, na=False)].values.tolist()
-    Item = pd.DataFrame(Item_value)
+    Item_value = IS_data[IS_data.iloc[:, 0].str.contains("Gross Profit", na=False)].values.tolist()
+    for Values in Item_value:
+        Item_value_final = [Values[:0:-1]]
+    Item = pd.DataFrame(Item_value_final)
     Item.to_csv(path + '\\' + "Fiscal_" + row_string + ".csv", mode='a', index=False, header=None, date_format='%Y%m%d')
